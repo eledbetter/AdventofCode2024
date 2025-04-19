547d1393593b7f76f231755f5e5f1e0b9c643457
Emma <e.lledbetter@gmail.com>
2025-04-18 22:43:39 -0500

    add day 1 solutions

---
+++ day_01_1.py
@@ -0,0 +1,31 @@
+# 1. import file
+
+inp = open("inputs/day_01.txt", "r")
+
+# 2. extract numbers from file, put into 2 lists
+#       - for each line:
+#       - split, add both #'s to corresponding list
+list1 = []
+list2 = []
+
+for line in inp:
+    split = line.split()
+    list1.append(int(split[0]))
+    list2.append(int(split[1]))
+
+inp.close()
+
+# 3. sort
+
+list1.sort()
+list2.sort()
+
+# 4. loop through & compare elements in lists, adding distance to total_distance
+
+total_distance = 0
+for i in range(0, len(list1)):
+    total_distance += abs(list1[i] - list2[i])
+
+# 5. print solution
+
+print(total_distance)
---
+++ day_01_2.py
@@ -0,0 +1,35 @@
+# 1. import file
+
+inp = open("inputs/day_01.txt", "r")
+
+# 2. extract numbers from file
+#       - loop thru each line
+#       - first num is added to hashmap as value
+#       - second num is added to list
+
+hashmap = {}
+rightList = []
+
+for line in inp:
+    split = line.split()
+    hashmap[int(split[0])] = 0
+    rightList.append(int(split[1]))
+
+inp.close()
+
+# 3. calc similarity score
+#       - loop thru list
+#       - if value in hashmap, ++ hashmap entry
+
+for num in rightList:
+    if num in hashmap:
+        hashmap[num] += 1
+
+simularity = 0
+
+for num in hashmap:
+    simularity += num * hashmap[num]
+
+# 4. print solution
+
+print(simularity)
